4b06271287a0fc94694a45b099a7a2633646d8a8
js0502 <jainamdhiren.shah@beacon.io>
2024-02-20 15:18:13 -0500

    useEffect midway

--- src/exercise/02.js
+++ src/exercise/02.js
@@ -6,20 +6,43 @@ import * as React from 'react'
 function Greeting({initialName = ''}) {
   // 🐨 initialize the state to the value from localStorage
   // 💰 window.localStorage.getItem('name') ?? initialName
-  const [name, setName] = React.useState(initialName)
+  // const [name, setName] = React.useState(window.localStorage.getItem('name') ?? initialName)
+ 
+  
 
+   console.log("Component rerender");
   // 🐨 Here's where you'll use `React.useEffect`.
   // The callback should set the `name` in localStorage.
   // 💰 window.localStorage.setItem('name', name)
 
+  const useLocalStorageState = (key, initialValue='') => {
+    const valInLocalStorage = window.localStorage.getItem(key);
+    const [variable, setVariable] = React.useState(()=> (valInLocalStorage?JSON.parse(valInLocalStorage):initialValue));
+     React.useEffect(()=>{
+      console.log( JSON.stringify({[key]:variable}))
+    window.localStorage.setItem(key, JSON.stringify(variable));
+  }, [key, variable]);
+  return [variable, setVariable];
+}
+
+  const [name, setName] = useLocalStorageState('name', initialName);
+
   function handleChange(event) {
     setName(event.target.value)
   }
+
+  // const [name2, setName2] = React.useState(initialName);
+  // function handleChange2(event) {
+  //   setName2(event.target.value)
+  // }
+
   return (
     <div>
       <form>
         <label htmlFor="name">Name: </label>
         <input value={name} onChange={handleChange} id="name" />
+        {/* <label htmlFor="name2">Name2: </label>
+        <input value={name2} onChange={handleChange2} id="name2" /> */}
       </form>
       {name ? <strong>Hello {name}</strong> : 'Please type your name'}
     </div>
@@ -27,7 +50,7 @@ function Greeting({initialName = ''}) {
 }
 
 function App() {
-  return <Greeting />
+  return <Greeting initialName={15}/>
 }
 
 export default App
